18e3982a09886e7824fea46e22b893a68630628a
sonnynajar <sonny.najar@gmail.com>
2020-01-29 14:49:42 -0800

    Create change-bg.rkt

---
+++ change-bg.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+(define racket-version
+  (version))
+
+(define home-path
+  (find-system-path 'home-dir))
+
+(displayln "Changing background...")
+
+(system (~a "DISPLAY=:0.0 xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/workspace0/last-image --set "
+        home-path ".racket/" racket-version "/pkgs/mc-scripts/img/mc-bg.png"))
+
+
+
