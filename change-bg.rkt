71f9bb136b94573172b0eb7ff93dc64d6143b408
sonnynajar <sonny.najar@gmail.com>
2020-01-30 09:37:38 -0800

    changed pngs

--- change-bg.rkt
+++ change-bg.rkt
@@ -10,6 +10,7 @@
 
 (system (~a "DISPLAY=:0.0 xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/workspace0/last-image --set "
         home-path ".racket/" racket-version "/pkgs/mc-scripts/img/mc-bg.png"))
+(system "raco mc-gui")
 
 
 
